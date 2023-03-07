b5d9bce64ecc2c7024302bae62dc9f625fcc1804
NorbertCiro <Salazarbritonorberto@gmail.com>
2023-03-06 22:07:07 -0500

    Add backend with node

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.get('/', (req, res) =>{
+  res.send('Server en express');
+})
+app.listen(port, () =>{
+  console.log('Aplicación escuchando en el puerto: '+port);
+});
